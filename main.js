dc66a8bbc0d76bd3de58c4cad9f8650c87a54904
Mohit-Jay-Patel <113121401+Mohit-Jay-Patel@users.noreply.github.com>
2023-07-24 19:23:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var video="";
+var nose_x='';
+var nose_y="";
+var model="";
+function preload() {
+	world_start = loadSound("world_start.wav");
+	jump = loadSound("jump.wav");
+	coin = loadSound("coin.wav");
+	kick = loadSound("kick.wav");
+	Game_over = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas");
+	instializeInSetup(mario);
+    
+	video= createCapture(VIDEO);
+	video.size(600,300);
+	video.parent("webcam");
+
+	model=ml5.poseNet(video,modelloaded);
+	model.on("pose", gotPoses);
+}
+function modelloaded(){
+	console.log("Model has been loaded");
+}
+function gotPoses(results){
+  if(results.length > 0){
+	console.log(results);
+	nose_x=results[0].pose.nose.x;
+	nose_y=results[0].pose.nose.y;
+	console.log("Nose X = "+nose_x+"Nose Y = "+nose_y);
+  }
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
